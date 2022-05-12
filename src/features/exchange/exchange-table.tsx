c3b184d68844cac516df9a4a056a0ad968068ba0
Jan Sedloň <jansedlon@icloud.com>
2022-05-12 20:33:34 +0200

    feat: add exchange table component

---
+++ src/features/exchange/exchange-table.tsx
@@ -0,0 +1,33 @@
+import { Table } from "~/ui/table/table";
+import { Card } from "~/ui/card";
+
+type Props = {
+  rates: {
+    kód: string;
+    množství: number;
+    kurz: number;
+  }[];
+};
+
+export function ExchangeTable({ rates }: Props) {
+  return (
+    <Card className="w-full">
+      <Table className="w-full">
+        <Table.Header>
+          <Table.Column>Code</Table.Column>
+          <Table.Column>Amount</Table.Column>
+          <Table.Column>Rate</Table.Column>
+        </Table.Header>
+        <Table.Body>
+          {rates.map((rate) => (
+            <Table.Row key={rate.kód}>
+              <Table.Cell>{rate.kód}</Table.Cell>
+              <Table.Cell>{rate.množství}</Table.Cell>
+              <Table.Cell>{rate.kurz}</Table.Cell>
+            </Table.Row>
+          ))}
+        </Table.Body>
+      </Table>
+    </Card>
+  );
+}
