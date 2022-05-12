d68be73964a41740bc2f68072bfa218076f6ac05
Jan Sedloň <jansedlon@icloud.com>
2022-05-12 17:18:19 +0200

    chore(ui): remove old styles and add Tailwind support

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { theme } from "./theme";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
-)
+);
