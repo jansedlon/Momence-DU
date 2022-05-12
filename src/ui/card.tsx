1eb4913234f166bf9a873143aa53b059a156e37f
Jan Sedloň <jansedlon@icloud.com>
2022-05-12 17:15:53 +0200

    feat(ui): card component

---
+++ src/ui/card.tsx
@@ -0,0 +1,19 @@
+import {
+  ComponentPropsWithoutRef,
+  ElementType,
+  PropsWithChildren,
+} from "react";
+import styled, { DefaultTheme, StyledComponentProps } from "styled-components";
+import * as React from "react";
+
+const StyledCard = styled.div`
+  box-shadow: ${({ theme }) => theme.shadows.md};
+  padding: 16px;
+  border-radius: 10px;
+`;
+
+export function Card<C extends string | React.ComponentType<any>>(
+  props: StyledComponentProps<C, DefaultTheme, any, any>
+) {
+  return <StyledCard {...props} />;
+}
