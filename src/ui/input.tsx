3bba943c588f362a0f1b5ea0c5132ce355c7f2b1
Jan Sedloň <jansedlon@icloud.com>
2022-05-12 17:16:09 +0200

    feat(ui): input component

---
+++ src/ui/input.tsx
@@ -0,0 +1,57 @@
+import { ComponentPropsWithoutRef, forwardRef } from "react";
+import styled from "styled-components";
+
+type Props = ComponentPropsWithoutRef<"input"> & {};
+
+const StyledInput = styled.input`
+  font-size: 1.5rem;
+  font-weight: bold;
+  font-family: "Inter", sans-serif;
+
+  border: 0;
+  border-radius: 0;
+  outline: 0;
+
+  transition: border-color 0.2s ease-in-out;
+`;
+
+const StyledInputContainer = styled.div`
+  display: inline-flex;
+  position: relative;
+
+  &::before {
+    content: "";
+    position: absolute;
+    bottom: 0;
+    width: 0;
+    height: 2px;
+    left: 50%;
+    transform: translate(-50%);
+    transition: width 0.2s ease-in-out;
+    background: black;
+    z-index: 2;
+  }
+
+  &:focus-within::before {
+    width: 100%;
+  }
+
+  &::after {
+    content: "";
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    height: 2px;
+    background: ${({ theme }) => theme.colors.gray["500"]};
+  }
+`;
+
+export const Input = forwardRef<HTMLInputElement, Props>(
+  ({ ...otherProps }, ref) => (
+    <StyledInputContainer>
+      <StyledInput ref={ref} {...otherProps} />
+    </StyledInputContainer>
+  )
+);
+
+Input.displayName = "Input";
