a8463d068b6e06ac66e3758b6cb7dd67940786d5
Jan Sedloň <jansedlon@icloud.com>
2022-05-12 20:32:30 +0200

    chore(ui): add switch icon

---
+++ src/icons/switch.tsx
@@ -0,0 +1,23 @@
+import { ComponentPropsWithoutRef, forwardRef } from "react";
+
+export const SwitchIcon = forwardRef<
+  SVGSVGElement,
+  ComponentPropsWithoutRef<"svg">
+>((props, ref) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    className="h-6 w-6"
+    fill="none"
+    viewBox="0 0 24 24"
+    stroke="currentColor"
+    strokeWidth={2}
+    {...props}
+    ref={ref}
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M8 7h12m0 0l-4-4m4 4l-4 4m0 6H4m0 0l4 4m-4-4l4-4"
+    />
+  </svg>
+));
